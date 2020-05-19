1b67b06ee1eddac9f9beaa8f0573f91fd998135c
Valdnet <47037905+Valdnet@users.noreply.github.com>
2020-05-19 11:30:41 +0200

    l10n: Addition of the dot character

--- ts/Manager/RoomRow.tsx
+++ ts/Manager/RoomRow.tsx
@@ -95,7 +95,7 @@ const RoomRow: React.FC<Props> = (props) => {
 		ev.preventDefault();
 
 		OC.dialogs.confirm(
-			t('bbb', 'Are you sure you want to delete "{name}"? This operation can not be undone', { name: room.name }),
+			t('bbb', 'Are you sure you want to delete "{name}"? This operation can not be undone.', { name: room.name }),
 			t('bbb', 'Delete "{name}"?', { name: room.name }),
 			confirmed => {
 				if (confirmed) {
