21789bf639416d041e57b1667f37cb8b16791c3f
sualko <klaus@jsxc.org>
2020-05-22 13:28:35 +0200

    l10n: change displayname
    fix #38

--- templates/publicdisplayname.php
+++ templates/publicdisplayname.php
@@ -14,9 +14,9 @@
 			<div class="warning"><?php p($l->t('The name must be at least 3 characters long.')); ?></div>
 		<?php endif; ?>
 		<p>
-			<label for="password" class="infield"><?php p($l->t('displayname')); ?></label>
+			<label for="password" class="infield"><?php p($l->t('Display name')); ?></label>
 			<input type="displayname" name="displayname" id="password"
-				placeholder="<?php p($l->t('displayname')); ?>" value=""
+				placeholder="<?php p($l->t('Display name')); ?>" value=""
 				required minlength="3" autofocus />
 			<input type="submit" id="displayname-submit"
 				class="svg icon-confirm input-button-inline" value="" />
