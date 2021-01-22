e9cd6aca20fc5f6b269c5146b5e447a9a3f09447
sualko <klaus@jsxc.org>
2021-01-22 19:19:43 +0100

    fix: translation of access options

--- ts/Common/Translation.ts
+++ ts/Common/Translation.ts
@@ -1,17 +1,9 @@
 import { Access } from './Api';
 
-interface EscapeOptions {
-	escape?: boolean;
-}
-
-export function bt(string: string, vars?: { [key: string]: string }, count?: number, options?: EscapeOptions): string {
-	return t('bbb', string, vars, count, options);
-}
-
 export const AccessOptions = {
-	[Access.Public]: bt('Public'),
-	[Access.Password]: bt('Internal + Password protection for guests'),
-	[Access.WaitingRoom]: bt('Internal + Waiting room for guests'),
-	[Access.Internal]: bt('Internal'),
-	[Access.InternalRestricted]: bt('Internal restricted'),
+	[Access.Public]: t('bbb', 'Public'),
+	[Access.Password]: t('bbb', 'Internal + Password protection for guests'),
+	[Access.WaitingRoom]: t('bbb', 'Internal + Waiting room for guests'),
+	[Access.Internal]: t('bbb', 'Internal'),
+	[Access.InternalRestricted]: t('bbb', 'Internal restricted'),
 };
