6ff3ab130b0ee62f3dcef49eb772fde3a1db693f
sualko <klaus@jsxc.org>
2021-12-08 13:32:21 +0100

    chore: enable app for unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,20 @@
 <?php
 
+if (!defined('PHPUNIT_RUN')) {
+	define('PHPUNIT_RUN', 1);
+}
+
 if (!($ncRoot = getenv('NEXTCLOUD_ROOT'))) {
 	$ncRoot = __DIR__ . '/../../..';
 }
 
-echo "Using ".realpath($ncRoot)." as Nextcloud root.\n\n";
-
 require_once $ncRoot . '/lib/base.php';
 require_once __DIR__ . '/../vendor/autoload.php';
+
+\OC_App::loadApp('bbb');
+
+if (!class_exists('\PHPUnit\Framework\TestCase')) {
+	require_once('PHPUnit/Autoload.php');
+}
+
+echo "Using ".realpath($ncRoot)." as Nextcloud root.\n\n";
