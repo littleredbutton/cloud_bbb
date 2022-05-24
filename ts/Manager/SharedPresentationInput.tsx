76bd1e16ab5b42d5faf73d81d907d29ff9fd7d09
Specht, David <DavidSpecht@protonmail.com>
2022-05-25 00:00:27 +0200

    feat: default Presentation
    eslint cleanup

--- ts/Manager/SharedPresentationInput.tsx
+++ ts/Manager/SharedPresentationInput.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { useState } from "react";
-import { Room } from "../Common/Api";
+import React from 'react';
+import { Room } from '../Common/Api';
 
 type Props = {
     id: string
@@ -11,38 +10,38 @@ type Props = {
 const SharedPresentationInput: React.FC<Props> = ({ room, updateRoom, id }) => {
 
 	function filepicker() {
-        OC.dialogs.filepicker(t('bbb', 'Default Presentation'), file => {
-            updateRoom({...room, presentationUserId: '', presentationPath: file});
-        },
-        );
-    }
+		OC.dialogs.filepicker(t('bbb', 'Default Presentation'), file => {
+			updateRoom({...room, presentationUserId: '', presentationPath: file});
+		},
+		);
+	}
 
-    function removeFile() {
-        updateRoom({...room, presentationUserId: '', presentationPath: ''});
-    }
+	function removeFile() {
+		updateRoom({...room, presentationUserId: '', presentationPath: ''});
+	}
 
-    function getAvatarUrl(userId) {
-        if (room.presentationUserId === null || room.presentationUserId === undefined) {
-            return ;
-        }
+	function getAvatarUrl() {
+		if (room.presentationUserId === null || room.presentationUserId === undefined) {
+			return ;
+		}
 
-        return (OC.generateUrl('/avatar/' + encodeURIComponent(room.presentationUserId) + '/' + 24, {
-            user: room.presentationUserId,
-            size: 24,
-            requesttoken: OC.requestToken,
-        }))
-    }
+		return (OC.generateUrl('/avatar/' + encodeURIComponent(room.presentationUserId) + '/' + 24, {
+			user: room.presentationUserId,
+			size: 24,
+			requesttoken: OC.requestToken,
+		}));
+	}
 
-    return(
-        <div className="bbb-presentation-input">
-            <input id={id} type="button" value={t('bbb', 'Choose a File')} onClick={filepicker} />
-            <p className={ room.presentationPath === '' ? 'hidden' : ''}>
-                <img src={getAvatarUrl(room.presentationUserId)} alt={room.presentationUserId} className="bbb-avatar" height="100%" />
-                <em>{room.presentationPath}</em>
-                <button onClick={removeFile}><span className="icon icon-close icon-visible"></span></button>
-            </p>
-        </div>
-    );
+	return(
+		<div className="bbb-presentation-input">
+			<input id={id} type="button" value={t('bbb', 'Choose a File')} onClick={filepicker} />
+			<p className={ room.presentationPath === '' ? 'hidden' : ''}>
+				<img src={getAvatarUrl()} alt={room.presentationUserId} className="bbb-avatar" height="100%" />
+				<em>{room.presentationPath}</em>
+				<button onClick={removeFile}><span className="icon icon-close icon-visible"></span></button>
+			</p>
+		</div>
+	);
 };
 
 export default SharedPresentationInput;
