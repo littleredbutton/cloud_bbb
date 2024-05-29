dd48d55419611b93ce1d66b34aea8c62ed41f36d
Sebastien Marinier <sebastien.marinier@arawa.fr>
2024-05-29 17:28:49 +0200

    feat: videos for all users and moderators
    Users and moderators recording views

--- ts/Manager/RoomRow.tsx
+++ ts/Manager/RoomRow.tsx
@@ -261,9 +261,7 @@ const RoomRow: React.FC<Props> = (props) => {
 				</td>
 				}
 				<td className="bbb-shrink">
-					{
-					<RecordingsNumber recordings={recordings} showRecordings={showRecordings} setShowRecordings={setShowRecordings} />
-					}
+				{<RecordingsNumber recordings={recordings} showRecordings={showRecordings} setShowRecordings={setShowRecordings} />}
 				</td>
 				<td className="clone icon-col">
 					<button className="action-item" onClick={cloneRow} title={t('bbb', 'Clone Room')}>
